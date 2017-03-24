b7a06891704439a87a2c6e366657d78283527caa
pradeepgangwar <pradeepgangwar39@gmail.com>
2017-03-24 11:00:46 +0530

    Added login functionality to website

--- index.php
+++ index.php
@@ -23,14 +23,14 @@
 		<input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
 		<div class="login-form">
 			<div class="sign-in-htm">
-
+    	<form class="login-form" method="POST" action="login.php">
 				<div class="group">
 					<label for="user" class="label">Username</label>
-					<input id="user" type="text" class="input">
+					<input id="user" type="text" class="input" name="user">
 				</div>
 				<div class="group">
 					<label for="pass" class="label">Password</label>
-					<input id="pass" type="password" class="input" data-type="password">
+					<input id="pass" type="password" class="input" data-type="password" name="pass">
 				</div>
 				<div class="group">
 					<input id="check" type="checkbox" class="check" checked>
@@ -43,6 +43,7 @@
 				<div class="foot-lnk">
 					<a href="#forgot">Forgot Password?</a>
 				</div>
+			</form>
 			</div>
 			<div class="sign-up-htm">
 				<div class="group">
